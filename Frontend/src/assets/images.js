411c21af8468849e827f0594fa39cf3fa2fc25b9
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-04-17 17:54:41 +0530

    feat(assets): add new food category images and export from images.js
    - Added Biryani, Desserts, Main Course, Salads, Starters, and Pizza & Pasta images
    - Created images.js to import and export all related food category images
    - Enabled centralized access to food category images in the frontend assets folder

---
+++ Frontend/src/assets/images.js
@@ -0,0 +1,15 @@
+import BiryaniGallary from './BIRYANI_IMAGE.jpg'
+import DessertGallary from './Desserts_image.jpg'
+import MainCourseGallary from './MAIN_COURSE_IMAGE.jpg'
+import SaladsGallary from './Salads_image.jpg'
+import StartersGallary from './Starters_image.jpg'
+import PastaPizzaGallary from './PIZZA_AND_PASTA.jpg'
+
+export {
+  BiryaniGallary,
+  DessertGallary,
+  MainCourseGallary,
+  SaladsGallary,
+  StartersGallary,
+  PastaPizzaGallary
+}
